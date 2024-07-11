b00ddf0275d44bb25bbdb69633d2b2cbeb3889dd
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-11 17:56:28 +0200

    Added exports for auth repository and models

--- lib/packages/authentication_repository/lib/authentication_repository.dart
+++ lib/packages/authentication_repository/lib/authentication_repository.dart
@@ -1,7 +1,2 @@
-library authentication_repository;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/authentication_repository.dart';
+export 'src/models/models.dart';
