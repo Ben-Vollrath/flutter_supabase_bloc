248a2d77a4beef2ebc5c81e2e67ef1da872d1c49
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-14 15:06:14 +0200

    Added tests for google sign in

--- lib/packages/authentication_repository/test/authentication_repository_test.dart
+++ lib/packages/authentication_repository/test/authentication_repository_test.dart
@@ -9,7 +9,7 @@ class MockCacheClient extends Mock implements CacheClient {}
 
 class MockSupabaseAuth extends Mock implements supabase.GoTrueClient {}
 
-class MockGoTrueClientUser extends Mock implements supabase.User{}
+class MockGoTrueClientUser extends Mock implements supabase.User {}
 
 class MockGoogleSignIn extends Mock implements GoogleSignIn {}
 
@@ -17,9 +17,10 @@ class MockGoogleSignInAccount extends Mock implements GoogleSignInAccount {}
 
 class MockAuthResponse extends Mock implements supabase.AuthResponse {}
 
-class MockGoogleSignInAuthentication extends Mock implements GoogleSignInAuthentication {}
+class MockGoogleSignInAuthentication extends Mock
+    implements GoogleSignInAuthentication {}
 
-void main(){
+void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   const email = "test@gmail.com";
@@ -36,50 +37,82 @@ void main(){
       supabaseAuth = MockSupabaseAuth();
       googleSignIn = MockGoogleSignIn();
       authenticationRepository = AuthenticationRepository(
-        cache: cacheClient, 
-        supabaseAuth: supabaseAuth, 
-        googleSignIn: googleSignIn
-      );
+          cache: cacheClient,
+          supabaseAuth: supabaseAuth,
+          googleSignIn: googleSignIn);
     });
 
     test('creates SupabaseAuth instance internally when not injected', () {
       expect(AuthenticationRepository.new, isNot(throwsException));
     });
 
-
     group('signUp', () {
       setUp(() {
-        when(
-          () => supabaseAuth.signUp(
-            email: email, 
-            password: password
-            )
-          ).thenAnswer((_) async => Future.value(MockAuthResponse()));
+        when(() => supabaseAuth.signUp(email: email, password: password))
+            .thenAnswer((_) async => Future.value(MockAuthResponse()));
       });
 
       test('calls signUp on SupabaseAuth', () async {
         await authenticationRepository.signUp(email: email, password: password);
-        verify(() => supabaseAuth.signUp(email: email, password: password)).called(1);
+        verify(() => supabaseAuth.signUp(email: email, password: password))
+            .called(1);
       });
 
       test('succeeds when signUp on SupabaseAuth succeeds', () async {
         expect(
-          authenticationRepository.signUp(email: email, password: password),
-          completes
-        );
+            authenticationRepository.signUp(email: email, password: password),
+            completes);
       });
 
-      test ('throws AuthFailure when signUp on SupabaseAuth fails', () async {
+      test('throws AuthFailure when signUp on SupabaseAuth fails', () async {
         when(() => supabaseAuth.signUp(email: email, password: password))
-          .thenThrow(Exception());
+            .thenThrow(Exception());
         expect(
-          authenticationRepository.signUp(email: email, password: password),
-          throwsA(isA<AuthFailure>())
-        );
+            authenticationRepository.signUp(email: email, password: password),
+            throwsA(isA<AuthFailure>()));
       });
-
     });
 
-  });
+    group('loginWithGoogle', () {
+      const accessToken = 'access-token';
+      const idToken = "id-token";
 
+      setUp(() {
+        final googleSignInAuthentication = MockGoogleSignInAuthentication();
+        final googleSignInAccount = MockGoogleSignInAccount();
+
+        when(() => googleSignIn.signIn())
+            .thenAnswer((_) async => googleSignInAccount);
+        when(() => googleSignInAccount.authentication)
+            .thenAnswer((_) async => googleSignInAuthentication);
+        when(() => googleSignInAuthentication.accessToken)
+            .thenReturn(accessToken);
+        when(() => googleSignInAuthentication.idToken).thenReturn(idToken);
+        when(() => supabaseAuth.signInWithIdToken(
+              provider: supabase.OAuthProvider.google,
+              idToken: "id-token",
+              accessToken: "access-token",
+            )).thenAnswer((_) async => MockAuthResponse());
+      });
+
+      test('calls signIn on GoogleSignIn', () async {
+        await authenticationRepository.logInWithGoogle();
+        verify(() => googleSignIn.signIn()).called(1);
+        verify(() => supabaseAuth.signInWithIdToken(
+            provider: supabase.OAuthProvider.google,
+            idToken: "id-token",
+            accessToken: "access-token")).called(1);
+      });
+
+      test('succeeds when signIn succeeds', () {
+        expect(authenticationRepository.logInWithGoogle(), completes);
+      });
+
+      test('throws AuthFailure when signIn fails', () {
+        when(() => googleSignIn.signIn()).thenThrow(Exception());
+        expect(authenticationRepository.logInWithGoogle(),
+            throwsA(isA<AuthFailure>()));
+      });
+    });
+  });
 }
