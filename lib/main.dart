9c7807e02aca29e42e4f5318159f9e1e16d3d3b5
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-14 17:26:37 +0200

    Added supabase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_supabase_bloc/firebase_options.dart';
+import 'package:flutter_supabase_bloc/packages/authentication_repository/lib/authentication_repository.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  //Bloc.observer = const AppBlocObserver();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL'].toString(),
+      anonKey: dotenv.env['SUPABASE_ANON_KEY'].toString());
+
+  final authenticationRepository = AuthenticationRepository();
+  await authenticationRepository.user.first;
+
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
