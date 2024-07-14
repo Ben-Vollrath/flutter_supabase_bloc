a710cc95ac93cdf4a10499a2dfdb7e702331061f
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-14 17:35:28 +0200

    Moved packages folder

---
+++ lib/app/view/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+
+class App extends StatelessWidget {
+  const App({
+    required AuthenticationRepository authenticationRepository,
+    super.key
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_supabase_bloc/firebase_options.dart';
-import 'package:flutter_supabase_bloc/packages/authentication_repository/lib/authentication_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
