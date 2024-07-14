4bc3fa52715ae2c453f05e1a93fc7322f42c8038
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-14 18:04:42 +0200

    Flutter create form inputs package

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/app_bloc.dart';
 
 class App extends StatelessWidget {
   const App({
@@ -11,6 +16,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AppBloc(
+          authenticationRepository: _authenticationRepository,
+        ),
+        child: AppView(),
+      ),
+    );
   }
 }
