f9a345e0ac7711e5b9d1dcea0fbb181b388ab1bc
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-14 18:35:12 +0200

    Added google sign in config

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_supabase_bloc/firebase_options.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/view/app.dart';
@@ -20,7 +21,13 @@ Future<void> main() async {
       url: dotenv.env['SUPABASE_URL'].toString(),
       anonKey: dotenv.env['SUPABASE_ANON_KEY'].toString());
 
-  final authenticationRepository = AuthenticationRepository();
+  final webClientId = dotenv.env['WEB_CLIENT_ID'];
+  final googleSignIn = GoogleSignIn(
+    serverClientId: webClientId,
+  );
+  final authenticationRepository = AuthenticationRepository(
+    googleSignIn: googleSignIn,
+  );
   await authenticationRepository.user.first;
 
   runApp(App(authenticationRepository: authenticationRepository));
