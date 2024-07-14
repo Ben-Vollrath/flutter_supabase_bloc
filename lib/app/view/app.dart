45739e68d74318560225466677c91f5628fad5bd
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-14 18:30:55 +0200

    Added appview

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,9 +1,11 @@
 import 'dart:ffi';
 
+import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../routes/routes.dart';
 import 'bloc/app_bloc.dart';
 
 class App extends StatelessWidget {
@@ -23,6 +25,20 @@ class App extends StatelessWidget {
           authenticationRepository: _authenticationRepository,
         ),
         child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FlowBuilder<AppStatus>(
+        state: context.select((AppBloc bloc) => bloc.state.status),
+        onGeneratePages: onGenerateAppViewPages,
       ),
     );
   }
